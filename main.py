0705a9f2101fe1ed9072987da96047a6fef2ef41
AkshaySG <akshay.sg55@gmail.com>
2025-11-16 18:17:56 +0000

    player changes, adding groups and modifying movement

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
-pygame.init()
+from player import Player
+
+
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    dt = 0
+    pygame.init()
+    clock_obj= pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+
     while True:
         log_state()
         ## below checks if user has closed the window
@@ -15,9 +26,16 @@ def main():
         for event in game_events:
             if event.type == pygame.QUIT:
                 return 
-        print(game_events)
         screen.fill(color="black")
+        # pauses the game loop until 1/60th a second has passed
+        time = clock_obj.tick(60)
+        # convert second to millisecond
+        dt = time/1000
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
+
 
 
 
